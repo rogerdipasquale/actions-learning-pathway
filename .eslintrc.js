0540d9c62cf5276d8c0dfc4d6e7f2b0c927c7cc6
Roger <rogerdipasquale@gmail.com>
2025-08-31 17:08:01 -0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+    // .eslintrc.js or similar
+    module.exports = {
+      extends: [
+        // ... other configurations
+        'plugin:@next/next/recommended',
+      ],
+      // ... other ESLint configurations
+    };
